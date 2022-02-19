400c55e86b5f54db42536cbd8b91d08631b80a4d
radsolav-marinov <radoslav.k.marinov@gmail.com>
2022-02-19 14:53:01 +0200

    Adopt Cloud message broker connectivity

--- hello-world/consumer.js
+++ hello-world/consumer.js
@@ -2,29 +2,36 @@ require('dotenv').config({
   path: require('path').resolve(__dirname, '../.env')
 });
 const {
+  AMP_PROTOCOL,
   APM_PORT = 5672,
   AMP_HOST = 'localhost',
-  AMQP_QUEUE_NAME
+  AMP_PATH,
+  AMP_QUEUE_NAME='test',
+  AMP_USER,
+  AMP_PASS,
 } = process.env;
 const amqp = require('amqplib');
 
+const connectUrl = `${AMP_PROTOCOL}://${AMP_USER}:${AMP_PASS}@${AMP_HOST}/${AMP_PATH}`
+console.log(connectUrl)
+
 async function consume() {
   try {
-    const connection = await amqp.connect(`amqp://${AMP_HOST}:${APM_PORT}`);
+    const connection = await amqp.connect( process.env.CLOUDAMQP_URL);
     const channel = await connection.createChannel();
 
     channel
-      .assertQueue(AMQP_QUEUE_NAME, {
+      .assertQueue(AMP_QUEUE_NAME, {
         durable: false
       })
       .then((res) => {})
       .catch((error) => console.error(error));
 
     channel.consume(
-      AMQP_QUEUE_NAME,
+      AMP_QUEUE_NAME,
       (msg) => {
         console.log(
-          `Message from ${AMQP_QUEUE_NAME} arrived: `,
+          `Message from ${AMP_QUEUE_NAME} arrived: `,
           JSON.parse(msg.content.toString())
         );
       },
--- hello-world/producer.js
+++ hello-world/producer.js
@@ -1,29 +1,35 @@
 require('dotenv').config({
   path: require('path').resolve(__dirname, '../.env')
 });
+
 const {
+  AMP_PROTOCOL,
+  AMP_PATH,
   APM_PORT = 5672,
   AMP_HOST = 'localhost',
-  AMQP_QUEUE_NAME
+  AMP_QUEUE_NAME='test',
+  AMP_USER,
+  AMP_PASS,
 } = process.env;
 
 const amqp = require('amqplib');
-
+const connectUrl = `${AMP_PROTOCOL}://${AMP_USER}:${AMP_PASS}@${AMP_HOST}/${AMP_PATH}`
+console.log(connectUrl)
 console.log(`Hello From Producer/Sender 😏`);
 
 producer();
 
 async function producer() {
   try {
-    const connection = await amqp.connect(`amqp://${AMP_HOST}:${APM_PORT}`);
+    const connection = await amqp.connect(process.env.CLOUDAMQP_URL);
     const channel = await connection.createChannel();
 
     channel
-      .assertQueue(AMQP_QUEUE_NAME, {
+      .assertQueue(AMP_QUEUE_NAME, {
         durable: false
       })
       .then((res) => {
-        console.log(`QUEUE "${AMQP_QUEUE_NAME}" is fine :)`, res);
+        console.log(`QUEUE "${AMP_QUEUE_NAME}" is fine :)`, res);
       })
       .catch((e) => {
         console.error(`QUEUE Error: `, e);
@@ -32,10 +38,10 @@ async function producer() {
     setInterval(() => {
       const message = { name: 'Riko', age: Math.ceil(Math.random() * 30) };
       const res = channel.sendToQueue(
-        AMQP_QUEUE_NAME,
+        AMP_QUEUE_NAME,
         Buffer.from(JSON.stringify(message), 'utf-8')
       );
-      console.log(`Message "${message} sent to queue "${AMQP_QUEUE_NAME}"`);
+      console.log(`Message "${message} sent to queue "${AMP_QUEUE_NAME}"`);
     }, 1000);
   } catch (error) {
     console.error(error);
